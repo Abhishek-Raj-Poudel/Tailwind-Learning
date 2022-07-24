f6272cc87023714977d3be7ebbaa3938398dd90b
unknown <abhishekrajpoudel@gmail.com>
2022-07-24 14:03:00 +0545

    Learned to make Cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jetbrains: ["JetBrains Mono", "monospace"],
+      },
+    },
   },
   plugins: [],
 };
